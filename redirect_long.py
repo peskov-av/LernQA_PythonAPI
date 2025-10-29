c6418ccda6f9d94cfda2bb39500baf8109a22c24
Andrey Peskov <andrey.peskov@restream.ru>
2025-10-29 14:02:11 +0300

    3

--- redirect_long.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-response = requests.get("https://playground.learnqa.ru/api/long_redirect")
-print(f"Количество редиректов: {len(response.history)}")
-print(f"Конечный URL: {response.url}")
